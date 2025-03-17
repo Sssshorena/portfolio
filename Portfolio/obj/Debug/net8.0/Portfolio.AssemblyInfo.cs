4516ae42806b29ebb151a17e8941331541b61572
Shorena Revazishvili <sssshorena@gmail.com>
2025-03-17 15:32:29 +0400

    update name

--- Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc5136aa90fb4837959911ed527ae9d44cdb946d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e9e549a290412715cd0ce65cbea80cc873663d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Portfolio/script.js
+++ Portfolio/script.js
@@ -6,7 +6,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     particlesJS("particles-js", {
         "particles": {
-            "number": { "value": 100, "density": { "enable": true, "value_area": 900 } }, 
+            "number": { "value": 100, "density": { "enable": true, "value_area": 900 } },
             "color": { "value": "#ffffff" },
             "shape": {
                 "type": "circle",
@@ -19,14 +19,14 @@ document.addEventListener("DOMContentLoaded", function () {
             "move": { "enable": true, "speed": 1.5, "direction": "none", "random": false, "straight": false, "out_mode": "out" }
         },
         "interactivity": {
-            "detect_on": "window", 
+            "detect_on": "window",
             "events": {
-                "onhover": { "enable": true, "mode": "repulse" }, 
-                "onclick": { "enable": true, "mode": "push" } 
+                "onhover": { "enable": true, "mode": "repulse" },
+                "onclick": { "enable": true, "mode": "push" }
             },
             "modes": {
                 "repulse": { "distance": 100, "duration": 0.4 },
-                "push": { "particles_nb": 5 } 
+                "push": { "particles_nb": 5 }
             }
         },
         "retina_detect": true
