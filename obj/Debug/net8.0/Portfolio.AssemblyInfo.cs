be75064108a439e744c9b66a292a240bcd430cf1
Shorena Revazishvili <sssshorena@gmail.com>
2025-03-17 21:35:46 +0400

    add changes

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cc7b0bd82cf05563de4ed6f7d9cf85441311715")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8284574e081a3299bf10d522f1aae31baabc0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
