e1f38d08f450308ff7135995d8176d2041472ca2
Shorena Revazishvili <sssshorena@gmail.com>
2025-03-17 15:00:03 +0400

    Resolved merge conflicts

--- Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab3806d9c615b37208b90ef258cb093633976680")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a24ce612b898cfd3c44adbd3d7f015e1796d9eed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
