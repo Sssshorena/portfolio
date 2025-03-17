a573636d97486348ffd3d25cadc12d3bc8d421d3
Shorena Revazishvili <sssshorena@gmail.com>
2025-03-17 15:07:52 +0400

    Removed wwwroot and moved static files to root

--- Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a24ce612b898cfd3c44adbd3d7f015e1796d9eed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1f38d08f450308ff7135995d8176d2041472ca2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
