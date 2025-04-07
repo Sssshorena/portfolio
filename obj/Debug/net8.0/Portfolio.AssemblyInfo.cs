972d5c9031cf3f678a20c7b9046863482542528c
Shorena Revazishvili <sssshorena@gmail.com>
2025-04-07 17:27:17 +0400

    update CV link

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8284574e081a3299bf10d522f1aae31baabc0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecba6dbc347865385bc711ef2b9de0763a199655")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
